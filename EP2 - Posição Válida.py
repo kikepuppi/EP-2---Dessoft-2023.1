1bdc19fd17c8177db70616dca58a4ba601e57ea0
Henrique Puppi <kikepuppi@gmail.com>
2023-04-19 19:44:59 -0300

    EP2 - Posição Válida completo

--- EP2 - Posição Válida.py
+++ EP2 - Posição Válida.py
@@ -32,18 +32,19 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             listresult.append([linha,coluna+i])
     return listresult
 def posicao_valida(frota, linha, coluna, orientacao, tamanho):
-    if frota == {}:
-        return False
-    for k , v in frota.items():
-        for k in frota.keys():
-            for i in range(len(v)):
-                for j in range (len(v[i])):
-                    if v[i][j] in define_posicoes(linha, coluna, orientacao, tamanho):
-                        return False
-    for valores in define_posicoes(linha, coluna, orientacao, tamanho):
+    x = define_posicoes(linha, coluna, orientacao, tamanho)
+    for valores in x:
         for numeros in valores:
             if valores[0] > 9 or valores[1] > 9:
                 return False
+    if frota == {}:
+        return True
+    for k , v in frota.items():
+        for i in range(len(v)):
+            for j in range (len(v[i])):
+                if v[i][j] in x:
+                    return False
+    
     return True
                     
 resultado = posicao_valida(frota, linha, coluna, orientacao, tamanho)
