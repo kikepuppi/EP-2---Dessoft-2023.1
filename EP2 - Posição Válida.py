7e5292c391e99154282664d48d32fc982bdf5b44
Henrique Puppi <kikepuppi@gmail.com>
2023-04-19 09:54:02 -0300

    Tentativas do EP2 - Posição Válida

---
+++ EP2 - Posição Válida.py
@@ -0,0 +1,52 @@
+# EP2 - Posição Válida
+
+frota = {
+    "navio-tanque":[
+      [[6,1],[6,2],[6,3]],
+      [[4,7],[5,7],[6,7]]
+    ],
+    "contratorpedeiro":[
+      [[1,1],[2,1]],
+      [[2,3],[3,3]],
+      [[9,1],[9,2]]
+    ],
+    "submarino": [
+      [[0,3]],
+      [[4,5]],
+      [[8,9]],
+      [[8,4]]
+    ],
+}
+linha = 8
+coluna = 8
+orientacao = 'horizontal'
+tamanho = 4
+
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    listresult = []
+    if orientacao == 'vertical':
+        for i in range (tamanho):
+            listresult.append([linha+i,coluna])
+    if orientacao == 'horizontal':
+        for i in range (tamanho):
+            listresult.append([linha,coluna+i])
+    return listresult
+
+print(define_posicoes(linha, coluna, orientacao, tamanho))
+
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+    if frota == {}:
+        return False
+    for k , v in frota.items():
+        for k in frota.keys():
+            for i in range(len(v)):
+                for j in range (len(v[i])):
+                    if v[i][j] in define_posicoes(linha, coluna, orientacao, tamanho):
+                        return False
+        if valores[0] > 10 or valores[1] > 10:
+            return False
+    return True
+                    
+resultado = posicao_valida(frota, linha, coluna, orientacao, tamanho)
+print(resultado)
+
