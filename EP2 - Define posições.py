64a0a7eae25289a1f566eedaf7fc55a3b266fe14
Henrique Puppi <kikepuppi@gmail.com>
2023-04-19 08:29:51 -0300

    Exercicio Define posições feito

--- EP2 - Define posições.py
+++ EP2 - Define posições.py
@@ -1,1 +1,17 @@
 # EP2 - Define posições
+linha = 2
+coluna = 4
+orientacao = "horizontal"
+tamanho = 3
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    listresult = []
+    if orientacao == 'vertical':
+        for i in range (tamanho):
+            listresult.append([linha+i,coluna])
+    if orientacao == 'horizontal':
+        for i in range (tamanho):
+            listresult.append([linha,coluna+i])
+    return listresult
+print(define_posicoes(linha, coluna, orientacao, tamanho))
+        
+
