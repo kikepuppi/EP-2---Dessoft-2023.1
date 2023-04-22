699efc6bd6793b147f35a9b0cc0e46fba9fe70f5
SidBarbosa <sidneybo@al.insper.edu.br>
2023-04-22 17:49:49 -0300

    finaluzação EP2 - afundados

---
+++ EP2 - afundados.py
@@ -0,0 +1,47 @@
+frota = {
+    "porta-aviões":[
+      [[1,5],[1,6],[1,7],[1,8]]
+    ],
+    "navio-tanque":[
+      [[6,1],[6,2],[6,3]],
+      [[4,7],[5,7],[6,7]]
+    ],
+    "contratorpedeiro":[
+      [[1,1],[2,1]],
+      [[2,3],[3,3]],
+      [[9,1],[9,2]]
+    ],
+    "submarino": [
+      [[0,3]],
+      [[4,5]],
+      [[8,9]],
+      [[8,4]]
+    ],
+}
+tabuleiro = [
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 'X', 'X', 'X', 'X', 0],
+  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+]
+def afundados(frota, tabuleiro):
+    total_afundados = 0
+    for navio, posições_gerais in frota.items():
+        for posição_umbarco in posições_gerais: 
+            afundado = True
+            for posição in posição_umbarco:
+                if tabuleiro[posição[0]][posição[1]] != 'X':
+                    afundado = False
+                    break
+            if afundado:
+                total_afundados += 1
+    return total_afundados
+
+resultado = afundados(frota, tabuleiro)
+print(resultado)
